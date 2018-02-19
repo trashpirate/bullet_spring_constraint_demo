d36728d84896f70424075c7699fc8d6bf35d340d
nadinaz <nadinaz@sent.com>
2018-02-18 21:45:22 -0600

    remove boost library

--- src/main_opengl.cpp
+++ src/main_opengl.cpp
@@ -26,7 +26,6 @@ subject to the following restrictions:
 
 #include <iostream>
 #include <vector>
-#include <boost/program_options.hpp>
 
 char* gVideoFileName = 0;
 char* gPngFileName = 0;
@@ -90,180 +89,130 @@ int main(int argc, char** argv)
 
   	try 
   	{ 
-    /** Define and parse the program options 
-     */ 
-	    namespace po = boost::program_options; 
-	    po::options_description desc("Options");
-	    desc.add_options() 
-	      ("help,h", "Help screen")
-	      ("SOLVER", po::value<int>(&param->SOLVER), "NNCG")
-	      ("DEBUG", po::value<int>(&param->DEBUG), "debug on/off")
-	      
-	      ("TIME_STEP", po::value<float>(&param->TIME_STEP), "time step")
-	      ("NUM_STEP_INT", po::value<int>(&param->NUM_STEP_INT), "number of internal steps")
-	      ("TIME_STOP", po::value<float>(&param->TIME_STOP), "time stop")
-		  
-		  ("SAVE_VIDEO", po::value<int>(&param->SAVE_VIDEO), "video on/off")
-	    
-		  
-		  ("file_video", po::value<std::string>(&param->file_video), "output file for simulation video");
-	 	
+    
+		SimpleOpenGL3App* app = new SimpleOpenGL3App("Bullet Whisker Simulation",1024,768,true);
+		
+		prevMouseButtonCallback = app->m_window->getMouseButtonCallback();
+		prevMouseMoveCallback = app->m_window->getMouseMoveCallback();
 
-	 	
-	    po::variables_map vm; 
+		app->m_window->setMouseButtonCallback((b3MouseButtonCallback)OnMouseDown);
+		app->m_window->setMouseMoveCallback((b3MouseMoveCallback)OnMouseMove);
+		
+		OpenGLGuiHelper gui(app,false);
+		CommonExampleOptions options(&gui);
+		
 
-	    // for(int i=0;i<param->stiffness_x.size();i++){
-    	// 	std::cout << param->stiffness_x[i] << std::endl;
-    	// }
+		simulation = SimulationCreateFunc(options);
+		simulation->processCommandLineArgs(argc, argv);
 
-	    try { 
-		    po::store(po::parse_command_line(argc, argv, desc), vm); // can throw 
-		 	po::notify(vm);
+		simulation->parameters = param; // save parameters in simulation object
 
-		 	if ( vm.count("help")  ) { 
-		        std::cout << "Bullet Whisker Simulation" << std::endl 
-		                  << desc << std::endl; 
-		        return 0; 
-		    } 
-		   
+		simulation->initPhysics();
+		simulation->resetCamera();
 
-	    	
+		char fileName[1024];
+		int textureWidth = 128;
+		int textureHeight = 128;
 
-		    // update_parameters(param);
+		unsigned char*	image = new unsigned char[textureWidth*textureHeight * 4];
+		int textureHandle = app->m_renderer->registerTexture(image, textureWidth, textureHeight);
+		
+		// int cubeIndex = app->registerCubeShape(1, 1, 1);
 
-			SimpleOpenGL3App* app = new SimpleOpenGL3App("Bullet Whisker Simulation",1024,768,true);
+		// b3Vector3 pos = b3MakeVector3(0, 0, 0);
+		// b3Quaternion orn(0, 0, 0, 1);
+		// b3Vector3 color = b3MakeVector3(1, 0, 0);
+		// b3Vector3 scaling = b3MakeVector3 (1, 1, 1);
+
+		if(param->SAVE_VIDEO){
+			std::string videoname = param->file_video;
+			gVideoFileName = &videoname[0];
 			
-			prevMouseButtonCallback = app->m_window->getMouseButtonCallback();
-			prevMouseMoveCallback = app->m_window->getMouseMoveCallback();
-
-			app->m_window->setMouseButtonCallback((b3MouseButtonCallback)OnMouseDown);
-			app->m_window->setMouseMoveCallback((b3MouseMoveCallback)OnMouseMove);
-			
-			OpenGLGuiHelper gui(app,false);
-			CommonExampleOptions options(&gui);
-			
-
-			simulation = SimulationCreateFunc(options);
-			simulation->processCommandLineArgs(argc, argv);
-
-			simulation->parameters = param; // save parameters in simulation object
-
-			simulation->initPhysics();
-			simulation->resetCamera();
-
-			char fileName[1024];
-			int textureWidth = 128;
-			int textureHeight = 128;
-	
-			unsigned char*	image = new unsigned char[textureWidth*textureHeight * 4];
-			int textureHandle = app->m_renderer->registerTexture(image, textureWidth, textureHeight);
-			
-			// int cubeIndex = app->registerCubeShape(1, 1, 1);
-	
-			// b3Vector3 pos = b3MakeVector3(0, 0, 0);
-			// b3Quaternion orn(0, 0, 0, 1);
-			// b3Vector3 color = b3MakeVector3(1, 0, 0);
-			// b3Vector3 scaling = b3MakeVector3 (1, 1, 1);
-
-			if(param->SAVE_VIDEO){
-				std::string videoname = param->file_video;
-				gVideoFileName = &videoname[0];
-				
-				if (gVideoFileName){
-					std::cout << "Rendering video..." << std::endl;
-					app->dumpFramesToVideo(gVideoFileName);
-	
-				}
-
-				std::string pngname = "png_test";
-				gPngFileName = &pngname[0];
-				
-				// app->m_renderer->registerGraphicsInstance(cubeIndex, pos, orn, color, scaling);
-				app->m_renderer->writeTransforms();
-
+			if (gVideoFileName){
+				std::cout << "Rendering video..." << std::endl;
+				app->dumpFramesToVideo(gVideoFileName);
 
 			}
-			
-			
 
-			do
-			{
-				if(param->SAVE_VIDEO){
-					static int frameCount = 0;
-					frameCount++;
+			std::string pngname = "png_test";
+			gPngFileName = &pngname[0];
+			
+			// app->m_renderer->registerGraphicsInstance(cubeIndex, pos, orn, color, scaling);
+			app->m_renderer->writeTransforms();
 
-					if (gPngFileName)
-					{
-						// printf("gPngFileName=%s\n", gPngFileName);
-		
-						sprintf(fileName, "%s%d.png", gPngFileName, frameCount++);
-						app->dumpNextFrameToPng(fileName);
-					}
+
+		}
 		
 		
-		
-					//update the texels of the texture using a simple pattern, animated using frame index
-					for (int y = 0; y < textureHeight; ++y)
-					{
-						const int	t = (y + frameCount) >> 4;
-						unsigned char*	pi = image + y*textureWidth * 3;
-						for (int x = 0; x < textureWidth; ++x)
-						{
-							const int		s = x >> 4;
-							const unsigned char	b = 180;
-							unsigned char			c = b + ((s + (t & 1)) & 1)*(255 - b);
-							pi[0] = pi[1] = pi[2] = pi[3] = c; pi += 3;
-						}
-					}
-		
-					app->m_renderer->activateTexture(textureHandle);
-					app->m_renderer->updateTexture(textureHandle, image);
-		
-					float color[4] = { 255, 1, 1, 1 };
-					app->m_primRenderer->drawTexturedRect(100, 200, gWidth / 2 - 50, gHeight / 2 - 50, color, 0, 0, 1, 1, true);
+
+		do
+		{
+			if(param->SAVE_VIDEO){
+				static int frameCount = 0;
+				frameCount++;
+
+				if (gPngFileName)
+				{
+					// printf("gPngFileName=%s\n", gPngFileName);
+	
+					sprintf(fileName, "%s%d.png", gPngFileName, frameCount++);
+					app->dumpNextFrameToPng(fileName);
 				}
+	
+	
+	
+				//update the texels of the texture using a simple pattern, animated using frame index
+				for (int y = 0; y < textureHeight; ++y)
+				{
+					const int	t = (y + frameCount) >> 4;
+					unsigned char*	pi = image + y*textureWidth * 3;
+					for (int x = 0; x < textureWidth; ++x)
+					{
+						const int		s = x >> 4;
+						const unsigned char	b = 180;
+						unsigned char			c = b + ((s + (t & 1)) & 1)*(255 - b);
+						pi[0] = pi[1] = pi[2] = pi[3] = c; pi += 3;
+					}
+				}
+	
+				app->m_renderer->activateTexture(textureHandle);
+				app->m_renderer->updateTexture(textureHandle, image);
+	
+				float color[4] = { 255, 1, 1, 1 };
+				app->m_primRenderer->drawTexturedRect(100, 200, gWidth / 2 - 50, gHeight / 2 - 50, color, 0, 0, 1, 1, true);
+			}
 
-				app->m_instancingRenderer->init();
-		    	app->m_instancingRenderer->updateCamera(app->getUpAxis());
+			app->m_instancingRenderer->init();
+			app->m_instancingRenderer->updateCamera(app->getUpAxis());
+		
+			simulation->stepSimulation();
+
+			simulation->renderScene();
 			
-				simulation->stepSimulation();
-
-				simulation->renderScene();
-				
-				app->m_renderer->renderScene();
-				DrawGridData dg;
-		        dg.upAxis = app->getUpAxis();
-				app->drawGrid(dg);
-				// char bla[1024];
-				// std::sprintf(bla, "Simple test frame %d", frameCount);
-				// app->drawText(bla, 10, 10);
-				app->swapBuffer();
+			app->m_renderer->renderScene();
+			DrawGridData dg;
+			dg.upAxis = app->getUpAxis();
+			app->drawGrid(dg);
+			// char bla[1024];
+			// std::sprintf(bla, "Simple test frame %d", frameCount);
+			// app->drawText(bla, 10, 10);
+			app->swapBuffer();
 
 
-			} while (!app->m_window->requestedExit() && !(simulation->exitFlag));
-			
-			std::cout << "Saving data..." << std::endl;
-			
+		} while (!app->m_window->requestedExit() && !(simulation->exitFlag));
+		
+		std::cout << "Saving data..." << std::endl;
+		
 
-			std::cout << "Exit simulation..." << std::endl;
-			simulation->exitPhysics();
-			delete simulation;
-			delete app;
-			delete[] image;
-			std::cout << "Done." << std::endl;
+		std::cout << "Exit simulation..." << std::endl;
+		simulation->exitPhysics();
+		delete simulation;
+		delete app;
+		delete[] image;
+		std::cout << "Done." << std::endl;
 
 
-	    } 
-	    catch(po::error& e) 
-	    { 
-	      std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
-	      std::cerr << desc << std::endl; 
-	      return 1; 
-	    } 
- 
-    // application code here // 
- 
-  	} 
+	}  
   	catch(std::exception& e) 
   	{ 
     	std::cerr << "Unhandled Exception reached the top of main: " 
@@ -277,9 +226,6 @@ int main(int argc, char** argv)
 } // main 
 
 
-
-// int main(int argc, char* argv[])
-// {
 
 
 
