cbe72b250c50714664b90ac6340f3db990fd2852
nadinaz <nadinaz@sent.com>
2019-06-21 13:21:12 -0500

    backup

---
+++ CMakeFiles/SpringConstraintDemo.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nadina/bullet3/examples/ExampleBrowser/CollisionShape2TriangleMesh.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/CollisionShape2TriangleMesh.cpp.o"
+  "/home/nadina/bullet3/examples/ExampleBrowser/GL_ShapeDrawer.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/GL_ShapeDrawer.cpp.o"
+  "/home/nadina/bullet3/examples/ExampleBrowser/OpenGLGuiHelper.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/OpenGLGuiHelper.cpp.o"
+  "/home/nadina/bullet3/examples/Importers/ImportObjDemo/Wavefront2GLInstanceGraphicsShape.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/Importers/ImportObjDemo/Wavefront2GLInstanceGraphicsShape.cpp.o"
+  "/home/nadina/bullet3/examples/OpenGLWindow/SimpleOpenGL3App.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/OpenGLWindow/SimpleOpenGL3App.cpp.o"
+  "/home/nadina/bullet3/examples/ThirdPartyLibs/stb_image/stb_image.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ThirdPartyLibs/stb_image/stb_image.cpp.o"
+  "/home/nadina/bullet3/examples/Utils/b3Clock.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/Utils/b3Clock.cpp.o"
+  "/home/nadina/Github/bullet_spring_constraint_demo/src/Parameters.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/src/Parameters.cpp.o"
+  "/home/nadina/Github/bullet_spring_constraint_demo/src/Pendulum.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/src/Pendulum.cpp.o"
+  "/home/nadina/Github/bullet_spring_constraint_demo/src/Simulation.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/src/Simulation.cpp.o"
+  "/home/nadina/Github/bullet_spring_constraint_demo/src/Simulation_utility.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/src/Simulation_utility.cpp.o"
+  "/home/nadina/Github/bullet_spring_constraint_demo/src/main_opengl.cpp" "/home/nadina/Github/bullet_spring_constraint_demo/CMakeFiles/SpringConstraintDemo.dir/src/main_opengl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "B3_USE_STANDALONE_EXAMPLE"
+  "GLEW_DYNAMIC_LOAD_ALL_GLX_FUNCTIONS=1"
+  "GLEW_INIT_OPENGL11_FUNCTIONS=1"
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/home/nadina/bullet3/src"
+  "/home/nadina/bullet3/btgui"
+  "/home/nadina/bullet3/examples"
+  "/home/nadina/bullet3/examples/ThirdPartyLibs/Glew"
+  "/home/nadina/bullet3/examples/ThirdPartyLibs"
+  "/home/nadina/bullet3/examples/OpenGLWindow"
+  "/home/nadina/bullet3/btgui/OpenGLWindow/GlewWindows"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/SpringConstraintDemo.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/OpenGLGuiHelper.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/GL_ShapeDrawer.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ExampleBrowser/CollisionShape2TriangleMesh.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/Utils/b3Clock.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/Importers/ImportObjDemo/Wavefront2GLInstanceGraphicsShape.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/OpenGLWindow/SimpleOpenGL3App.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/home/nadina/bullet3/examples/ThirdPartyLibs/stb_image/stb_image.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/src/Parameters.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/src/Simulation_utility.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/src/Simulation.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/src/Pendulum.cpp.o"
+  "CMakeFiles/SpringConstraintDemo.dir/src/main_opengl.cpp.o"
+  "SpringConstraintDemo.pdb"
+  "SpringConstraintDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpringConstraintDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
